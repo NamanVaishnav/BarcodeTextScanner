05d31bf287f3588301c2d519bc5f253a525bf6bb
Naman Vaishnav <namanmvaishnav@gmail.com>
2023-12-22 13:00:23 +0530

    add completion of barcode and text scanning functionality

--- BarcodeTextScanner/AppViewModel.swift
+++ BarcodeTextScanner/AppViewModel.swift
@@ -11,7 +11,7 @@ import Foundation
 import SwiftUI
 import VisionKit
 
-enum ScanType {
+enum ScanType: String {
     case text, barcode
 }
 
@@ -34,6 +34,24 @@ class AppViewModel: ObservableObject {
     
     var recognizedDataType: DataScannerViewController.RecognizedDataType {
         scanType == .barcode ? .barcode() : .text(textContentType: textContentType)
+    }
+    
+    var headerText: String {
+        if recognizedItems.isEmpty {
+            return "Scanning \(scanType.rawValue)"
+        } else {
+            return "Recognized \(recognizedItems.count) item(s)"
+        }
+    }
+    
+    var dataScannerViewId: Int {
+        var hasher = Hasher()
+        hasher.combine(scanType)
+        hasher.combine(recognizesMulitpleItems)
+        if let textContentType {
+            hasher.combine(textContentType)
+        }
+        return hasher.finalize()
     }
     
     private var isScannerAvailiable: Bool {
--- BarcodeTextScanner/ContentView.swift
+++ BarcodeTextScanner/ContentView.swift
@@ -6,10 +6,20 @@
 //
 
 import SwiftUI
+import VisionKit
 
 struct ContentView: View {
     
-    @EnvironmentObject var vm: AppViewModel 
+    @EnvironmentObject var vm: AppViewModel
+    private let textContentType: [(title: String,textContentType: DataScannerViewController.TextContentType?)] = [
+        ("All", .none),
+        ("URL", .URL),
+        ("Phone", .telephoneNumber),
+        ("Email", .emailAddress),
+        ("Address",.fullStreetAddress)
+    ]
+    
+    
     var body: some View {
         switch vm.dataScannerAccessStatus {
         case .noDetermined:
@@ -19,7 +29,7 @@ struct ContentView: View {
         case .cameraNotAvailable:
             Text("Your device don't have camera")
         case .scannerAvailable:
-            Text("Scanner is Available")
+            mainView
         case .scannerNotAvailable:
             Text("Your device don't have support for scanning barcode with camera")
         }
@@ -30,6 +40,77 @@ struct ContentView: View {
             recognizedItems: $vm.recognizedItems,
             recognizedDataType: vm.recognizedDataType,
             recognizedMultipleItems: vm.recognizesMulitpleItems)
+        .background {
+            Color.gray.opacity(0.3)
+        }
+        .ignoresSafeArea()
+        .id(vm.dataScannerViewId)
+        .sheet(isPresented: .constant(true), content: {
+            bottomContainerView
+                .background(.ultraThinMaterial)
+                .presentationDetents([.medium,.fraction(0.25)])
+                .presentationDragIndicator(.visible)
+                .interactiveDismissDisabled()
+                .onAppear{
+                    guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene,
+                          let controller = windowScene.windows.first?.rootViewController?.presentedViewController else {
+                        return
+                    }
+                    controller.view.backgroundColor = .clear
+                }
+        })
+        .onChange(of: vm.scanType) { _ in vm.recognizedItems = [] }
+        .onChange(of: vm.textContentType) { _ in vm.recognizedItems = [] }
+        .onChange(of: vm.recognizesMulitpleItems) { _ in vm.recognizedItems = [] }
+    }
+    
+    private var headerView: some View {
+        VStack {
+            HStack {
+                Picker("Scan Type", selection: $vm.scanType) {
+                    Text("Barcode").tag(ScanType.barcode)
+                    Text("Text").tag(ScanType.text)
+                }.pickerStyle(.segmented)
+                
+                Toggle("Scan Multiple", isOn: $vm.recognizesMulitpleItems)
+                
+            }
+            .padding(.top)
+            
+            if vm.scanType == .text {
+                Picker("Text content type", selection: $vm.textContentType){
+                    ForEach(textContentType, id: \.self.textContentType){ option in
+                        Text(option.title).tag(option.textContentType)
+                    }
+                }.pickerStyle(.segmented)
+            }
+            
+            Text(vm.headerText).padding(.top)
+            
+        }
+        .padding(.horizontal)
+    }
+    
+    private var bottomContainerView: some View {
+        VStack{
+            headerView
+            ScrollView {
+                LazyVStack(alignment: .leading , spacing: 16, content: {
+                    ForEach(vm.recognizedItems) { item in
+                        switch item {
+                        case .barcode(let barcode):
+                            Text(barcode.payloadStringValue ?? "Unknown barcode")
+                        case .text(let text):
+                            Text(text.transcript)
+                        @unknown default:
+                            Text("Unnknown")
+                            
+                        }
+                    }
+                })
+                .padding()
+            }
+        }
     }
 }
 
